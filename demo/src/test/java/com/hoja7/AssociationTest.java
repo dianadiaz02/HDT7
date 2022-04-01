1081823df52353195902fe398e58f6fa4ec8ba2b
dianadiaz02 <80539135+dianadiaz02@users.noreply.github.com>
2022-03-31 23:06:06 -0600

    test

---
+++ demo/src/test/java/com/hoja7/AssociationTest.java
@@ -0,0 +1,12 @@
+package com.hoja7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssociationTest {
+
+    @Test
+    void compareTo() {
+    }
+}
---
+++ demo/src/test/java/com/hoja7/BinarySearchTreeTest.java
@@ -0,0 +1,20 @@
+package com.hoja7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+
+    @Test
+    void getRoot() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void traverseInOrder() {
+    }
+}
